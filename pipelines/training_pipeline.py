ccee9cffe9da5e0628d336d5dedc5456ef3a9e9b
Jordan Delbar <jordandelbar@gmail.com>
2022-12-19 23:30:05 +0100

    Add: autoflake in pre-commit hook

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -6,4 +6,4 @@ def training_pipeline(loader, splitter, trainer):
     # Link all the steps artifacts together
     train, target, test = loader()
     X_train, X_test, y_train, y_test = splitter(train, target)
-    clf_pipeline = trainer(X_train, y_train)
+    clf_pipeline = trainer(X_train, y_train)  # noqa: F841
