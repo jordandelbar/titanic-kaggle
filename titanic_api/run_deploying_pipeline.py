cbec3a81d5933b204aa4f45441b9b7df6f36b74e
Jordan Delbar <jordan.delbar@adeo.com>
2023-01-09 14:23:26 +0100

    Wip: draft of deploying pipeline

--- titanic_api/run_deploying_pipeline.py
+++
@@ -1,9 +0,0 @@
-from pipelines.deploying_pipeline import deploying_pipeline
-from steps.bento_builder import bento_builder
-from steps.model_fetcher import model_fetcher
-
-run = deploying_pipeline(model_fetcher=model_fetcher(), bento_builder=bento_builder)
-
-
-if __name__ == "__main__":
-    run.run()
--- titanic_api/service.py
+++ titanic_api/service.py
@@ -10,18 +10,18 @@ svc = bentoml.Service("titanic-service", runners=[titanic_runner])
 
 
 class TitanicFeatures(BaseModel):
-    Pclass: int
-    Sex: str
-    Age: float
-    Fare: float
-    Embarked: str
-    is_baby: int
-    alone: int
-    family: int
-    title: str
+    Pclass: int = 3
+    Sex: str = "male"
+    Age: float = 33.0
+    Fare: float = 8.0
+    Embarked: str = "Q"
+    is_baby: int = 0
+    alone: int = 0
+    family: int = 2
+    title: str = "Mr"
 
 
 @svc.api(input=JSON(pydantic_model=TitanicFeatures), output=NumpyNdarray())
 def predict_bentoml(input_data: TitanicFeatures) -> numpy.ndarray:
     input_df = pandas.DataFrame([input_data.dict()])
-    return titanic_runner.predict.run(input_df)
+    return titanic_runner.predict_proba.run(input_df)[:, 1]
