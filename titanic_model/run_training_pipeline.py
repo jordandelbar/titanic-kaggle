ba6329b37fcb3605443c4daccb30db4ab982c433
Jordan Delbar <jordandelbar@gmail.com>
2023-02-20 23:01:38 +0100

    Ref: change preprocessor to processing to avoid confusion

--- titanic_model/run_training_pipeline.py
+++ titanic_model/run_training_pipeline.py
@@ -1,11 +1,12 @@
 """Run the training pipeline."""
 from pipelines.training_pipeline import training_pipeline
 from steps.data_loader.pandas_data_loader import data_loader
-from steps.data_preprocessing.pandas_preprocessor import preprocessor
 from steps.data_splitter.sklearn_data_splitter import data_splitter
 from steps.model_evaluator.evaluator import model_evaluator
 from steps.model_register.register import model_register
 from steps.model_trainer.pytorch_trainer import trainer
+
+from titanic_model.steps.data_processing.pandas_processing import preprocessor
 
 run = training_pipeline(
     loader=data_loader(),
