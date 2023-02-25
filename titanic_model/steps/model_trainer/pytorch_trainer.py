ebf6ee2cb51b0ea44b932d4ec3e5ef55b71f099f
Jordan Delbar <jordandelbar@gmail.com>
2023-02-25 18:46:42 +0100

    Add: custom mlflow model

--- titanic_model/steps/model_trainer/pytorch_trainer.py
+++ titanic_model/steps/model_trainer/pytorch_trainer.py
@@ -10,7 +10,11 @@ from zenml.integrations.mlflow.flavors.mlflow_experiment_tracker_flavor import (
 from zenml.steps import Output, step
 
 from titanic_model.config.core import config
-from titanic_model.model_definition.pytorch_model_pipeline import titanic_pipeline
+from titanic_model.model_definition.pytorch_model_pipeline import (
+    LogisticRegression,
+    MeanImputer,
+    TargetEncoding,
+)
 
 experiment_tracker = Client().active_stack.experiment_tracker
 
@@ -42,12 +46,45 @@ def trainer(
     mlflow.log_dict(config.dict(), "model_config.json")
     # mlflow.sklearn.autolog(log_input_examples=True)
 
-    clf_pipeline = titanic_pipeline.fit(X=X_train, y=y_train)
+    mean_imputer = MeanImputer()
+    target_encoder = TargetEncoding(m=300)
+    clf = LogisticRegression(input_dim=9, output_dim=1, epochs=5000)
 
-    mlflow.sklearn.log_model(
-        sk_model=clf_pipeline,
+    x = target_encoder.fit_transform(
+        x=X_train,
+        y=y_train,
+        features_list=[
+            "Sex",
+            "Embarked",
+            "Pclass",
+            "is_baby",
+            "alone",
+            "family",
+            "title",
+        ],
+    )
+    x = mean_imputer.fit_transform(x=x, features_list=["Age"])
+    clf.fit(x=x.to_numpy(), y=y_train)
+
+    class CustomModel(mlflow.pyfunc.PythonModel):
+        def __init__(self, mean_imputer, target_encoder, clf):
+            self.mean_imputer = mean_imputer
+            self.target_encoder = target_encoder
+            self.clf = clf
+
+        def predict(self, context, model_input):
+            x_test = self.target_encoder.transform(x=model_input)
+            x_test = self.mean_imputer.transform(x=x_test)
+            return self.clf.predict(x=x_test.to_numpy())
+
+    model = CustomModel(
+        mean_imputer=mean_imputer, target_encoder=target_encoder, clf=clf
+    )
+
+    mlflow.pyfunc.log_model(
+        python_model=model,
         artifact_path="model",
         input_example=X_train.loc[0].to_dict(),
     )
 
-    return clf_pipeline
+    return model
